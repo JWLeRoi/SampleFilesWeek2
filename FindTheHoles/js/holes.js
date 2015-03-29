1473427b1d0c1c08772d28905b4267840a581709
JW LeRoi <jack.leroi@ucdenver.edu>
2015-03-29 11:34:20 -0600

    2015.03.29.11:18
    holes.html
    Added text-center Bootstrap class for HTML elements. Added label. Added comments.
    
    holes.js
    Removed debugging code. Refactored inputText.keypress. Added comments.
    
    holes_backup.js
    Removed.

--- FindTheHoles/js/holes.js
+++ FindTheHoles/js/holes.js
@@ -2,87 +2,93 @@
  * Created by j.LeRoi on 3/25/2015.
  */
 
-//use of const was characterized with Chrome Version 41.0.2272.101 m
-const strArrHOLES = ["WwETtYyrUuIiSsFfGHhJjKkLlZzXxCcVvNnMm12357", "4690QeqROoPpAaDdb", "B"];
-/*//var result = findChar("w")
-//alert(result);
-alert("Before 0");
+/*
+ Define constant array to search for the number of holes in the tested character.
+ Characters in element 0 have 0 holes. Characters in element 1 have 1 hole. Characters in element 2 have 2 holes.
+ Use of const was characterized with Chrome Version 41.0.2272.101 m
+ */
+const strArrHOLES = ["WwETtYyrUuIiSsFfGHhJjKkLlZzXxCcVvNnMm12357", "4690QeqRO#@oPpAaDdb", "%&B"];
 
-if (strArrHOLES[0].includes("B"))
-{
-    alert("0");
-
-}
-
-alert("Before 1");
-if (strArrHOLES[1].includes("B"))
-{
-    alert("1");
-
-}
-
-alert("Before 2");
-if (strArrHOLES[2].includes("B"))
-{
-    alert("2");
-
-}
-*/
-
-
+//Use JQuery to delay execution until the page is ready.
 $(document).ready(function()
 {
+    /*Set up variables.*/
+    /*Use JQuery to get handles for HTML elements.*/
     var submitButton = $("#btnSubmit");
     var inputText    = $("#inBox");
     var outputText   = $("#outBox");
 
+    var count;
 
-
+    //Use JQuery to set up handler for click event in entry box.
     inputText.on("click", function()
     {
-        //inputText.val("");
-        //alert("inputText - click");
         inputText.val("");
         outputText.val("");
     });
 
+    //Use JQuery to set up handler for Enter key press in entry box.
     inputText.keypress(function(event)
     {
+        //Test for the Enter key.
         if (event.which === 13)
         {
-            var inputStrArray = inputText.val().split("");
-            //alert(inputStrArray);
-            var result = inputStrArray.map(findChar);
-            var count  = result.reduce(function(runningTotal, currentValue)
+            //Count the number of holes in the input string.
+            count  = inputText.val().split("").map(findChar).reduce(function(runningTotal, currentValue)
             {
                 return runningTotal + currentValue;
             });
 
-            //alert(count);
+            /*Select the output for the number of holes.*/
+            if (count === 1)
+            {
+                //Display the output message for 1 hole.
+                outputText.val("There is " + count + " hole in the string.");
+            }
+            else
+            {
+                //Display the output message for 0 holes or more than 1 hole.
+                outputText.val("There are " + count + " holes in the string.");
+            }
 
-            outputText.val("There are " + count + " holes in the string.");
-
-            //alert("Here");
-            //alert("inputText - keypress")
         }
 
     });
 
 });
 
+/*Test the submitted character for the number of holes.*/
 function findChar(inChar)
 {
-    //alert("Here");
-    for (var index = 0; index < 3; index += 1)
-     {
-     //alert(index + " " + inChar);
-         if (strArrHOLES[index].includes(inChar))
-         {
-            //alert(index + " " + inChar);
-             return index;
+    /*
+     Loop through the constant array for the number of holes.
+     The constant array has 3 elements.
+     */
+    for (var index = 0; index < 4; index += 1)
+    {
+        /*
+         If the index reaches 3, the submitted character is not found in the constant array
+         and it is assumed the submitted character has 0 holes.
+         */
+        if (index === 3)
+        {
+            return 0;
+        }
 
-         }
+        /*
+         If the submitted character matches one of the characters in the 0 element
+         of the constant array, the submitted character has 0 holes. Return the 0 index.
+         If the submitted character matches one of the characters in the 1 element
+         of the constant array, the submitted character has 1 hole. Return the 1 index.
+         If the submitted character matches one of the characters in the 2 element
+         of the constant array, the submitted character has 2 holes. Return the 2 index.
+         */
+        if (strArrHOLES[index].includes(inChar))
+        {
+            return index;
 
-     }
+        }
+
+    }
 
 };
